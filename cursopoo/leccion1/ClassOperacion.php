590bf12bc0bdeb7d918dfda8c541d1aed8886e99
zagaz1979 <cesar.garcia.suarez@gmail.com>
2021-09-27 17:04:51 -0500

    creando los metodos

--- cursopoo/leccion1/ClassOperacion.php
+++ cursopoo/leccion1/ClassOperacion.php
@@ -7,6 +7,36 @@
         public $cantidaddos = 0;
         public $resultado = 0;
 
+        //método constructor 
+        function __construct($intcant1, $intcant2){
+            $this->cantidaduno = $intcant1;
+            $this->cantidaddos = $intcant2;
+        }
+
+        //método getSuma()
+        public function getSuma(){
+            $this->resultado = $this->cantidaduno + $this->cantidaddos;
+            return $this->resultado;
+        }
+
+        //método getResta()
+        public function getResta(){
+            $this->resultado = $this->cantidaduno - $this->cantidaddos;
+            return $this->resultado;
+        }
+
+        //método getMultiplicacion()
+        public function getMultiplicacion(){
+            $this->resultado = $this->cantidaduno * $this->cantidaddos;
+            return $this->resultado;
+        }
+
+        //método getDivision)
+        public function getDivision(){
+            $this->resultado = $this->cantidaduno / $this->cantidaddos;
+            return $this->resultado;
+        }
+
 
     }//end class Operacion
 
